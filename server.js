37b6f800d99f43786411c39960da727613849dbe
aricallen <aric.allen2@gmail.com>
2018-09-11 09:45:39 -0700

    fix route strings

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const HarExpress = require('./har-express.js');
 
 const app = express();
 app.use(bodyParser.json());
+
+HarExpress(app);
 
 app.listen(4242);
--- utils/har-parser.js
+++ utils/har-parser.js
@@ -9,17 +9,12 @@ const harData = JSON.parse(fs.readFileSync('stubs/hars/login-request.har', { enc
 const initial = [{ request: {}, response: {} }];
 
 const model = harData.log.entries.reduce((acc, curr) => {
-  const request = {
-    method: curr.request.method,
-    url: curr.request.url,
-  };
-  const response = {
-    headers: curr.response.headers,
-    content: curr.response.content,
-  };
+  const { request, response } = curr;
   acc.push({ request, response });
   return acc;
 }, initial);
+
+let isInitialWrite = true;
 
 model.forEach((rr) => {
   // write out an express method and response
@@ -27,7 +22,16 @@ model.forEach((rr) => {
     const method = rr.request.method.toLowerCase();
     const path = rr.request.url;
     const response = JSON.stringify(rr.response.content, null, 2);
-    const output = `app.${method}('${path}', () => (${response}));\n\n`;
-    fs.appendFileSync(outputFile, output);
+    const output = `app.${method}('${path}', (req, res) => (
+      res.send(
+        ${response}
+      )
+    );\n\n`;
+    if (isInitialWrite === true) {
+      fs.writeFileSync(outputFile, output, 'utf8');
+      isInitialWrite = false;
+    } else {
+      fs.appendFileSync(outputFile, output);
+    }
   }
 });
