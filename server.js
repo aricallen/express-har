ef774a82ac0bdd1020ece4d1ed16cc53294bcdd4
aricallen <aric.allen2@gmail.com>
2018-09-11 21:23:52 -0700

    refactor to wildcard endpoints

--- server.js
+++ server.js
@@ -1,10 +1,60 @@
 const express = require('express');
+const session = require('express-session');
 const bodyParser = require('body-parser');
-const HarExpress = require('./har-express.js');
+const fs = require('fs');
+
+const harData = JSON.parse(fs.readFileSync('__hars__/localhost.har', { encoding: 'utf8' }));
 
 const app = express();
 app.use(bodyParser.json());
+app.use(
+  session({
+    secret: 'abc123',
+    cookie: { maxAge: 60000 },
+    saveUninitialized: true,
+  })
+);
 
-HarExpress(app);
+const getResponseData = (method, req) => {
+  const endpointEntry = harData.log.entries.find((entry) => {
+    if (
+      entry.request &&
+      entry.request.method.toLowerCase() === method &&
+      entry.request.url.includes(req.path)
+    ) {
+      return true;
+    }
+    return false;
+  });
+  if (endpointEntry !== undefined) {
+    return JSON.parse(endpointEntry.response.content.text);
+  }
+  return {};
+};
+
+app.get('/api/login', (req, res) => {
+  const data = getResponseData('post', req);
+  res.send(data);
+});
+
+app.get('/*', (req, res) => {
+  const data = getResponseData('get', req);
+  res.send(data);
+});
+
+app.post('/*', (req, res) => {
+  const data = getResponseData('post', req);
+  res.send(data);
+});
+
+app.put('/*', (req, res) => {
+  const data = getResponseData('put', req);
+  res.send(data);
+});
+
+app.delete('/*', (req, res) => {
+  const data = getResponseData('delete', req);
+  res.send(data);
+});
 
 app.listen(4242);
