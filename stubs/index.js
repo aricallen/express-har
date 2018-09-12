9c90fd50c1203b09436623ef4e232aa432ba648a
aricallen <aric.allen2@gmail.com>
2018-09-11 21:33:06 -0700

    more cleanup

--- stubs/index.js
+++
@@ -1,7 +0,0 @@
-const customerSettings = require('./customerSettings.js');
-const applicationSettings = require('./applicationSettings.js');
-
-module.exports = {
-  customerSettings,
-  applicationSettings,
-};
